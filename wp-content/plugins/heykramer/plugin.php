132ebf6a86cb2eeb3dc049e49f40c523ba7d4801
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-02-27 12:01:21 -0600

    hey kramer theme

--- wp-content/plugins/heykramer/plugin.php
+++ wp-content/plugins/heykramer/plugin.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Hey Kramer
- * Description: Hey Kramer WordPress Plugin
+ * Description: Hey Kramer - REST API for Seinfeld junkies
  * Author: Andy Brudtkuhl
  * Author URI: http://youmetandy.com
  * Version: 0.1
@@ -11,13 +11,48 @@
  */
 
 
- add_action( 'rest_api_init', function () {
-     register_rest_route( 'api', '/quotes', array(
-         'methods' => 'GET',
-         'callback' => 'get_quotes',
-     ) );
- } );
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'api', '/quotes', array(
+        'methods'   => 'GET',
+        'callback'  => 'get_quotes',
+    ) );
 
- function get_quotes() {
-     return get_posts( array( 'category' => 'quotes' ) );
- }
+    register_rest_route( 'api', '/quotes/random', array(
+        'methods'   => 'GET',
+        'callback'  => 'get_random_quote',
+    ) );
+
+    register_rest_route( 'api', '/gifs', array(
+        'methods'  =>  'GET',
+        'callback' =>  'get_gifs',
+    ) );
+
+    register_rest_route( 'api', '/gifs/random', array(
+        'methods'  =>  'GET',
+        'callback' =>  'get_random_gif',
+    ) );
+} );
+
+function get_quotes() {
+    return get_posts( array( 'category_name' => 'quotes' ) );
+}
+
+function get_random_quote() {
+    return get_posts( array(
+        'category_name'     =>  'quotes',
+        'orderby'           =>  'rand',
+        'posts_per_page'    =>  1,
+    ) );
+}
+
+function get_gifs() {
+    return get_posts( array( 'category_name' => 'gifs' ) );
+}
+
+function get_random_gif() {
+    return get_posts( array(
+        'category_name'     =>  'gifs',
+        'orderby'           =>  'rand',
+        'posts_per_page'    =>  1,
+    ) );
+}

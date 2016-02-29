cd4cd5d3497e38bcca3a303eff7759762c76ad90
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-02-28 22:16:50 -0600

    slacky slack app button

--- wp-content/themes/heykramer/footer.php
+++ wp-content/themes/heykramer/footer.php
@@ -11,14 +11,13 @@
 </div> <!-- /#content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-<?php // substitute the class "container-fluid" below if you want a wider content area ?>
 	<div class="container">
 		<div class="row">
 			<div class="site-footer-inner col-sm-12">
 
 				<div class="site-info">
 					<?php do_action( '_tk_credits' ); ?>
-					built by <a href="http://brudtkuhl.com">@abrudtkuhl</a>
+					<a href="/">heykramer</a> / <a href="/api">API</a> / <a href"/slack">slack bot</a> / built by <a href="http://brudtkuhl.com">@abrudtkuhl</a>
 				</div><!-- close .site-info -->
 
 			</div>
--- wp-content/themes/heykramer/parts/homepage-hero.php
+++ wp-content/themes/heykramer/parts/homepage-hero.php
@@ -3,5 +3,6 @@
         <h1>Hey Kramer</h1>
         <h3>A Slack Bot</h3>
         <h4>( and seinfeld api )</h4>
+        <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=23393793856.23394255239"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>
     </div>
 </div>

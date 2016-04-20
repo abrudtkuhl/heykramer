3d2f6240750ade9d3e932f31bf2230462996d6f0
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-04-20 15:54:46 -0500

    footer

--- wp-content/themes/heykramer/footer.php
+++ wp-content/themes/heykramer/footer.php
@@ -9,7 +9,7 @@
 ?>
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-	built by <a href="http://youmetandy.com" target="_blank">@abrudtkuhl</a> / <a href="https://brudtkuhl.com/topics/heykramer/" target="_blank">about</a> / <a href="/wp-json/api">API</a> / <a href="https://github.com/abrudtkuhl/heykramerbot" target="_blank"><i class="fa fa-github-alt"></i></a>
+	built by <a href="http://youmetandy.com" target="_blank">@abrudtkuhl</a> <br /> <a href="https://brudtkuhl.com/topics/heykramer/" target="_blank">about</a> / <a href="/wp-json/api">API</a> / <a href="https://github.com/abrudtkuhl/heykramerbot" target="_blank"><i class="fa fa-github-alt"></i></a>
 </footer><!-- close #colophon -->
 
 <?php wp_footer(); ?>

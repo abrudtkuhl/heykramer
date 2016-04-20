7c0edf541002553f010438f3b766c8c37a74227b
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-04-20 16:15:35 -0500

    MOAR THEME UPDATES

--- wp-content/themes/heykramer/front-page.php
+++ wp-content/themes/heykramer/front-page.php
@@ -3,7 +3,7 @@
 <div id="hero">
     <div id="hero-text">
         <h1>Hey Kramer</h1>
-        <h3>A Slack Bot For Seinfeld Fans</h3>
+        <h3>A Slack Bot For Seinfeld Enthusiasts</h3>
         <a href="https://slack.com/oauth/authorize?scope=incoming-webhook,commands,bot&client_id=23393793856.23394255239"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>
     </div>
 </div>

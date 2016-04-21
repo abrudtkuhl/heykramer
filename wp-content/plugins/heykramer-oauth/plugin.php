6fae4f0db056f9576c2b09f7779372337bf5b1f7
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-04-20 21:35:46 -0500

    Slack API OAuth plugin to handle bot installation

---
+++ wp-content/plugins/heykramer-oauth/plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Plugin Name: Hey Kramer OAuth
+ * Description: Hey Kramer - Slack OAuth Integration
+ * Author: Andy Brudtkuhl
+ * Author URI: http://youmetandy.com
+ * Version: 0.1
+ * Plugin URI: http://github.com/abrudtkuhl/heykramer
+ * License: GPL2+
+ */
+
+add_action( 'rest_api_init', function () {
+    register_rest_route( 'api', '/oauth', array(
+        'methods'   =>  'GET',
+        'callback'  =>  'get_oauth',
+    ) );
+} );
+
+function get_oauth() {
+    $code = $_GET['code'];
+
+    $response = wp_remote_post( 'https://slack.com/api/oauth.access',
+        array(
+            'body'  =>  array( 'client_id' => SLACK_CLIENT_ID, 'client_secret' => SLACK_CLIENT_KEY, 'code' => $code ),
+        )
+    );
+
+    $user = $response['body'];
+}

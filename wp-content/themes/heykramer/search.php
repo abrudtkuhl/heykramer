6920f4e18b1d926ac5a522310e646c0eb56d9c50
Andy Brudtkuhl <abrudtkuhl@pork.org>
2016-05-06 16:04:56 -0500

    archive and search result page templates

--- wp-content/themes/heykramer/search.php
+++ wp-content/themes/heykramer/search.php
@@ -6,27 +6,32 @@
  */
 
 get_header(); ?>
+<div class="container">
+	<div class="col-md-8">
+		<?php if ( have_posts() ) : ?>
 
-	<?php if ( have_posts() ) : ?>
+			<header>
+				<h2 class="page-title"><?php printf( __( 'Search Results for: %s', '_tk' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+			</header><!-- .page-header -->
 
-		<header>
-			<h2 class="page-title"><?php printf( __( 'Search Results for: %s', '_tk' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
-		</header><!-- .page-header -->
+			<?php // start the loop. ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php // start the loop. ?>
-		<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'search' ); ?>
 
-			<?php get_template_part( 'content', 'search' ); ?>
+			<?php endwhile; ?>
 
-		<?php endwhile; ?>
+			<?php _tk_content_nav( 'nav-below' ); ?>
 
-		<?php _tk_content_nav( 'nav-below' ); ?>
+		<?php else : ?>
 
-	<?php else : ?>
+			<?php get_template_part( 'no-results', 'search' ); ?>
 
-		<?php get_template_part( 'no-results', 'search' ); ?>
+		<?php endif; // end of loop. ?>
+	</div>
+	<div class="col-md-4">
+		<?php get_sidebar(); ?>
+	</div>
+</div>
 
-	<?php endif; // end of loop. ?>
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
